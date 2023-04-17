4611b65ecbc1003afa9727e7c62d408f42695de3
Pandu Wijaya <wijayapandu388@gmail.com>
2023-04-17 12:49:51 +0700

    paging adapter

---
+++ app/src/main/java/com/example/themovieapp/ui/adapter/MovieAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.themovieapp.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.themovieapp.R
+import com.example.themovieapp.data.remote.Movie
+import com.example.themovieapp.databinding.ItemMovieBinding
+
+class MovieAdapter: PagingDataAdapter<Movie, MovieAdapter.MovieViewHolder>(COMPARATOR) {
+    inner class MovieViewHolder(private val binding: ItemMovieBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(movie: Movie){
+            with(binding){
+                Glide.with(itemView).load("${movie.baseUrl}${movie.poster_path}")
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .error(R.drawable.ic_error)
+                    .into(ivMoviePoster)
+
+                tvMovieTitle.text = movie.original_title
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        if (currentItem != null){
+            holder.bind(currentItem)
+        }
+    }
+
+    companion object{
+        private val COMPARATOR = object: DiffUtil.ItemCallback<Movie>(){
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
